e3dc534820b0e1aa906037c54b743cd3eee070b8
Mehdi Aminazadeh <mehdi.2000.aminazadeh@gmail.com>
2022-05-04 03:42:42 +0430

    last commit

--- Project1.py
+++ Project1.py
@@ -1,27 +1,20 @@
 import re
 from datetime import datetime
 
+
 def convertDate(strDate):
     format = "%A %d %B %Y %H:%M"
     date = datetime.strptime(strDate, format)
-    return date.timestamp()*1000
+    return date.timestamp() * 1000
 
 
-with open('2018-December.txt', 'r') as f:
+with open('2018-December.txt', 'rt') as f:
     Lines = f.read()
 
     with open('data1.txt', 'w') as w:
-        
         lsInput = re.sub("\n|\d?.=|(?<=Int).", "", Lines)
         lsInput = re.sub("\s+", ",", lsInput)
         lsInput = re.sub("(\w+)day|Int", "\n", lsInput)
-        
+
         for i in lsInput:
             w.writelines(i)
-
-
-
-        # d = []
-        # for i in file2:
-        #     if re.findall('^[A-Z]+.+',i,re.M):
-        #         d.append(i)
