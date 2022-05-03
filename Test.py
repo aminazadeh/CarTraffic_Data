b76b779e70553cef9cbd10a797c7a778c68ceb0f
alisa <alisafavi23@gmail.com>
2022-05-03 23:13:51 +0430

    test

--- Test.py
+++ Test.py
@@ -3,6 +3,7 @@ while(True):
     print(i)
     break
 
-print("hi i'm ali safavi ")
-print("hi i'm ali safavi ")
-print("hi i'm ali safavi ")
+print("hi i'm ali safavi 1")
+print("hi i'm ali safavi 2")
+print("hi i'm ali safavi 3")
+print("hi i'm ali safavi 4")
