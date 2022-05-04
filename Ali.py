eff51955e119f13a76080a35b5388b8f8ff166a5
alisa <alisafavi23@gmail.com>
2022-05-04 13:52:09 +0430

    clear data v3

--- Ali.py
+++ Ali.py
@@ -1,9 +1,11 @@
+import csv
 import re
 from datetime import datetime
 
 fileDir = "C:\\Users\\alisa\\Desktop\\rawData.txt"
 with open(fileDir, "rt") as input:
     lsInput = input.read()
+    input.close()
 
 lsInput = re.sub("\n|\d?.=|(?<=Int).", "", lsInput)
 lsInput = re.sub("\s+", ",", lsInput)
@@ -11,13 +13,29 @@ lsInput = re.sub("(\w+)day|Int", "\n", lsInput)
 
 
 def convertDate(strDate):
-    format = "%A,%d,%B,%Y,%H:%M"
+    format = ",%d,%B,%Y,%H:%M"
     date = datetime.strptime(strDate, format)
     return date.timestamp() * 1000
 
+lsInput = lsInput.split()
+for i, row in enumerate(lsInput):
+    if row[0] == ",":
+        date = convertDate(row)
+        lsInput.remove(row)
+        lsInput[i] = lsInput[i][:5] + "%f,"%date + lsInput[i][6:]
+        continue
+    lsInput[i]=row[:5]+"%f,"%date+row[6:]
 
-# for row in lsInput:
-#     print(row)
+def getArr():
+    return lsInput
 
-print(lsInput)
-# print(convertDate("Saturday,01,December,2018,00:15"))
+def getCsv(dir):
+    with open(dir,'w') as f:
+        write = csv.writer(f)
+
+        # write.writerow(fields)
+        write.writerows(lsInput)
+
+
+
+getCsv("C:\\Users\\alisa\\Desktop\\out.csv")
